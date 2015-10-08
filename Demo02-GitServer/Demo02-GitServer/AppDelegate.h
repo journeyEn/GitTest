5e830438138ea638abef6086bb5f04318cb1d812
journey <journey@journey.local>
2015-10-08 11:55:39 +0800

    测试

---
+++ Demo02-GitServer/Demo02-GitServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo02-GitServer
+//
+//  Created by journey on 15/10/8.
+//  Copyright (c) 2015年 journey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
